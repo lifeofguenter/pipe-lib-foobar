4f8dd7659f478b38c4e127bf99bcec6668a6147d
Gunter Grodotzki <gunter.grodotzki@prodigyfinance.com>
2018-10-04 14:09:28 +0200

    bar

--- vars/foobar.groovy
+++ vars/foobar.groovy
@@ -1,12 +1,12 @@
-@NonCPS
+//@NonCPS
 def call() {
-  def causes = currentBuild.rawBuild.getCauses()
-
-  for (cause in causes) {
-    if (cause instanceof hudson.triggers.TimerTrigger.TimerTriggerCause) {
-      echo 'time triggered #3'
-    }
-  }
+  //def causes = currentBuild.rawBuild.getCauses()
+  //
+  //for (cause in causes) {
+  //  if (cause instanceof hudson.triggers.TimerTrigger.TimerTriggerCause) {
+  //    echo 'time triggered #3'
+  //  }
+  //}
 
   try {
     sh 'php --version'
