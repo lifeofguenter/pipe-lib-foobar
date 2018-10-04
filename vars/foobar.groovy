2d6ebcefdfc77ea86ee2f87cd21c5c318f3674b6
Gunter Grodotzki <gunter.grodotzki@prodigyfinance.com>
2018-10-04 12:10:19 +0200

    grind my gears #3

--- vars/foobar.groovy
+++ vars/foobar.groovy
@@ -1,11 +1,10 @@
-@NonCPS
-
 def call() {
   def causes = currentBuild.rawBuild.getCauses()
 
-  for (cause in causes) {
-    if (cause instanceof hudson.triggers.TimerTrigger.TimerTriggerCause) {
-      echo 'time triggered'
+  //for (cause in causes) {
+  for (int i = causes.size(); i < max; i++) {
+    if (causes[i] instanceof hudson.triggers.TimerTrigger.TimerTriggerCause) {
+      echo 'time triggered #2'
     }
   }
 
