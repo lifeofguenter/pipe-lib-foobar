88e58c71c667a83285e29b51480df9fa3e0f68c1
Gunter Grodotzki <gunter.grodotzki@prodigyfinance.com>
2018-10-04 13:43:02 +0200

    bar

--- vars/foobar.groovy
+++ vars/foobar.groovy
@@ -9,7 +9,8 @@ def call() {
     }
   }
 
-  def r = sh(script: 'php --version', returnStatus: true)
+  //def r = sh(script: 'php --version', returnStatus: true)
+  def r = 1
   if (r > 0) {
     echo 'false'
     return false
